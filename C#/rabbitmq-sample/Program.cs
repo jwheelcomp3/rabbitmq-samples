5f15f1556c215994dd1feaae9b5016cbd0427859
Jared Wheeler <jwheelcomp3@gmail.com>
2019-05-28 21:30:04 -0400

    Files

---
+++ C#/rabbitmq-sample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using RabbitMQ.Net;
+
+namespace rabbitmq_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+
+                }
+            }
+        }
+    }
+}
